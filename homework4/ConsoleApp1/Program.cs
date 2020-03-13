70d0b9b857cf953275e241e7e0a17c3eb8ec29c9
展麒 邓 <429503410@qq.com>
2020-03-13 13:45:05 +0800

    add homework4

---
+++ homework4/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    public delegate void Tick(object sender,int time);
+    public delegate void Alarm(object sender,int time);
+    public class Clock
+    {
+        public event Tick tick;
+        public event Alarm alarm;
+        public int time=0;
+        public void ClockGo()
+        {
+            while(true)
+            {
+                tick(this,time);
+                if (time % 24 == 8)                              //达到条件（8：00），触发alarm
+                alarm(this,time);
+                time += 1;
+                Thread.Sleep(1000);
+            } 
+        }
+    }
+    public class createClock
+    {
+        public Clock clock = new Clock();
+        public createClock()
+            {
+            clock.tick += tick1;                                   //事件订阅
+            clock.alarm += alarm1;                                
+            }
+            void tick1(object sender, int time)
+        {
+            Console.WriteLine($"the clock is ticking {time % 24}:00");              //时钟走时发出信息
+        }
+        void alarm1(object sender, int time)
+        {
+            Console.WriteLine($"叮铃叮铃，现在的时间是 {time % 24}:00！");             //响铃信息
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            createClock clockTest = new createClock();
+            clockTest.clock.ClockGo();                                //闹钟启动
+        }
+    }
+}
---
+++ homework4/ConsoleApp5/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Node<T>                         //泛型结点
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>                   //泛型链表
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if(tail==null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void Foreach(Action<T> action)
+        {
+            for (Node<T> x = head; x != null; x = x.Next)
+                action(x.Data);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+            }
+            int maximun = -10000, minimun = 10000, sum = 0;
+            list.Foreach(data => Console.WriteLine(data));
+            list.Foreach(data => { if (data > maximun) maximun = data; });
+            list.Foreach(data => { if (data < minimun) minimun = data; });
+            list.Foreach(data => sum += data );
+            Console.WriteLine($"maximun:{maximun}, minimun:{minimun}, sum:{sum}");
+        }
+    }
+}
