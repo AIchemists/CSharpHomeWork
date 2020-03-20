f1703f6d7425a704f974c717f08d79ef43a3333e
展麒 邓 <429503410@qq.com>
2020-03-21 00:56:54 +0800

    add homework5

---
+++ homework5/ConsoleApp6/商品.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price;                       //商品价格
+        public Product(string name,double price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public Product()
+        {
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ homework5/ConsoleApp6/客户.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public Client(string name,string address)
+        {
+            this.Name = name;
+            this.Address = address;
+        }
+        public override string ToString()
+        {
+            return  Name + "  地址：" + Address;
+        }
+    }
+}
