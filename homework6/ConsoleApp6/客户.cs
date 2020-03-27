1f37c922deaa48b36f5f3929b53acddc33a9348b
展麒 邓 <429503410@qq.com>
2020-03-27 23:30:58 +0800

    add homework6

---
+++ homework6/ConsoleApp6/客户.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    [Serializable]
+    public  class Client
+    {
+        public string Name;
+        public string Address;
+        public Client(){}
+        public Client(string name,string address)
+        {
+            this.Name = name;
+            this.Address = address;
+        }
+        public override string ToString()
+        {
+            return  Name + "  地址：" + Address;
+        }
+    }
+}
