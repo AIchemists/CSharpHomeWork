6146dc896276ff0a1752ea040ff06dfeae2e33b0
展麒 邓 <429503410@qq.com>
2020-04-30 20:26:05 +0800

    add homework10

---
+++ homework10/WindowsFormsApp5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [DllImport("kernel32.dll")]
+        public static extern Boolean AllocConsole();
+
+        [STAThread]
+        static void Main()
+        {
+          //  AllocConsole();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
